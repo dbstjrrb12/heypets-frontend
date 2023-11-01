4458de891b2b8765c35e19af7b756a18d66f5b7b
윤석규 <sg.yun@joongonara.co.kr>
2023-11-01 17:37:42 +0900

    [fix] : 선택된 펫 전역 상태 정의

--- src/context/atom/home.atom.ts
+++ src/context/atom/home.atom.ts
@@ -1,7 +1,13 @@
+import { Pet } from '@/api/pet/list';
 import Day from '@/utils/day';
 import { atom } from 'recoil';
 
 export const selectedDay = atom<string>({
   key: 'SELECTED_DAY',
   default: new Day().format('YYYY-MM-DD'),
+});
+
+export const selectedPet = atom<Pet | undefined>({
+  key: 'SELECTED_PET_ID',
+  default: undefined,
 });
